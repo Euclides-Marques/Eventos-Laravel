02f595be6a6e4441a9b16ac33913d3e09a789357
Euclides-Marques <euclidesmarques270@gmail.com>
2024-01-07 18:03:02 -0300

    A9

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        <!-- GOOGLE FONTES -->
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+        <!-- BOOTSTRAP APLICAÇÃO -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <!-- CSS APLICAÇÃO -->
+        <link rel="stylesheet" href="/css/styles.css">
+
+        <!-- JAVASCRIPT APLICAÇÃO -->
+        <script src="/js/script.js"></script>
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="collapse navbar-collapse" id="navbar">
+                    <a href="/" class="navbar-brand">
+                        <img src="/img/logo.png" alt="Logo">
+                    </a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/events/create" class="nav-link">Criar Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Entrar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Cadastrar</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        @yield('content')
+        <footer>
+            <p>Marques Events &copy; 2024</p>
+        </footer>
+        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    </body>
+</html>
---
+++ resources/views/product.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title', 'Produto')
+
+@section('content')
+    <p>Exibindo o Produto de ID {{ $id }}</p>
+@endsection
